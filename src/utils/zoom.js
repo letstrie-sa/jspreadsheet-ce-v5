48a1a1c40e4ef671ed68fc48c2becd64d17a841e
MD. Sakib Khan <sakibkhan111296@gmail.com>
2025-03-04 15:59:08 +0600

    Refactor zoom utility functions to enforce scale limits and improve formatting

--- src/utils/zoom.js
+++ src/utils/zoom.js
@@ -1,10 +1,12 @@
-export const setZoom = function(scale) {
-    const obj = this;
+export const setZoom = function (scale) {
+  const obj = this;
+  if (scale >= 0.5 && scale <= 3) {
     obj.content.style.zoom = scale;
-}
+  }
+};
 
-export const getZoom = function() {
-    const obj = this;
-    const zoomLevel = obj.content.style.zoom
-    return zoomLevel? parseFloat(zoomLevel) : 1;
-}
+export const getZoom = function () {
+  const obj = this;
+  const zoomLevel = obj.content.style.zoom;
+  return zoomLevel ? parseFloat(zoomLevel) : 1;
+};
